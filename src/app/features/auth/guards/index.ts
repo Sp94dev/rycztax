a9f7a228c1bd768f4ee84923ab8e1f5537ea0931
sp94dev <sp94dev@gmail.com>
2025-07-11 22:56:19 +0200

    chore(app): Move Angular application to

--- src/app/features/auth/guards/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './isAuthenticated.guard';
-export * from './isNotAuthenticated.guard';
--- src/app/features/home/home.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { LoginPageComponent } from 'auth/pages/login-page.component';
-
-@Component({
-  selector: 'app-home',
-  imports: [LoginPageComponent],
-  template: ` <app-login-page /> `,
-})
-export class HomeComponent {}
